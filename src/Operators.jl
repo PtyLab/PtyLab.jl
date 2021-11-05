f4dfd0eef0fda2397eaecefc41d9a8b6a50c1a25
roflmaostc <fxw+git@mailbox.org>
2021-11-05 12:44:05 -0400

    Clean Fraunhofer operator

--- src/Operators.jl
+++ src/Operators.jl
@@ -1,30 +1,53 @@
 export Fraunhofer
 
 """
-    Fraunhofer(arr, rec, params; shift=false, dims=(1,2))
+    Fraunhofer(arr, rec::Reconstruction, params::Parameters; shift=false, dims=(1,2), FFTW_flags=FFTW.MEASURE)
 
 Returns two functions `object2detector, detector2object` which can propagate `asw` with Fraunhofer assumption
 efficiently between object and detector back and forth
-
+Currently uses `plan_fft!` for in-place ffts. `FFTW_flags` is only passed to `plan_fft` if the array is a CPU array.
 
  ## Examples
+ `arr` should be something like the ESW `rec.object[1:rec.Np, 1:rec.Np, ..] .* rec.probe`.
 ```julia-repl
-julia> 
+julia> o2d, d2o = Fraunhofer(arr, rec, params)
 ```
 """
-function Fraunhofer(arr, rec, params; shift=true, dims=(1,2))
-    p = plan_fft(arr, dims)
-  
+function Fraunhofer(arr::T, rec::Reconstruction, params::Params; dims=(1,2), FFTW_flags=FFTW.MEASURE) where T
+
+    # planning can overwritte array sometimes!
+    # only FFTW allows different flags
+    p = if T <: CuArray
+            plan_fft!(similar(arr), dims)
+        else
+            plan_fft!(similar(arr), dims, flags=FFTW_flags)
+    end
+
+    # those let blocks are required because of 
+    # https://docs.julialang.org/en/v1/manual/performance-tips/#man-performance-captured
+
+
     # create object to detector 
-    o2d(x) = p * x
+    o2d = let p=p
+        o2d(x) = p * x
+    end
     # same with shift
-    o2ds(x) = fftshift(p * ifftshift(x, dims), dims)
+    o2ds = let p=p
+        o2ds(x) = fftshift(p * ifftshift(x, dims), dims)
+    end
 
-    # create detector to object 
-    d2o(x) = inv(p) * x 
-    d2os(x) = fftshift(inv(p) * ifftshift(x, dims), dims)
+    # create detector to object without shifts 
+    d2o = let p=p
+        d2o(x) = inv(p) * x 
+    end
 
-    if shift
+    d2os = let p=p
+        d2os(x) = fftshift(inv(p) * ifftshift(x, dims), dims)
+    end
+
+
+    # return correct functions
+    if params.fftshiftFlag
         return o2ds, d2os
     else
         return o2d, d2o
