423fdf711e87b1b958a0a1f2f502f604a56d41ab
roflmaostc <fxw+git@mailbox.org>
2021-11-19 21:39:07 -0500

    Add more tests

--- test/utils_display.jl
+++ test/utils_display.jl
@@ -4,4 +4,10 @@
     @test PtyLab.gray_show([2, -1], set_one = true) == ColorTypes.Gray{Float64}[Gray{Float64}(1.0), Gray{Float64}(-0.5)]
     @test PtyLab.gray_show([1, -1], set_zero = true) == ColorTypes.Gray{Float64}[Gray{Float64}(1.0), Gray{Float64}(0.0)]
     @test PtyLab.gray_show([1, -1], set_one = true) == ColorTypes.Gray{Float64}[Gray{Float64}(1.0), Gray{Float64}(-1.0)]
+
+
+    grr = grid_regular_rand((4,4), (2,2), (2,2), 0)
+
+    @test show_grid(grr) == Gray{Float64}[Gray{Float64}(1.0) Gray{Float64}(1.0) Gray{Float64}(1.0) Gray{Float64}(0.0); Gray{Float64}(1.0) Gray{Float64}(1.0) Gray{Float64}(1.0) Gray{Float64}(0.0); Gray{Float64}(1.0) Gray{Float64}(1.0) Gray{Float64}(1.0) Gray{Float64}(0.0); Gray{Float64}(0.0) Gray{Float64}(0.0) Gray{Float64}(0.0) Gray{Float64}(0.0)]
+
 end
