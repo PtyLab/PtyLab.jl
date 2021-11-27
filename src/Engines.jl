0c4ca2a8dfac33b7a9bdd492255f5c52d42b7a14
roflmaostc <fxw+git@mailbox.org>
2021-11-27 05:28:06 +0100

    Some cleaning of comments

--- src/Engines.jl
+++ src/Engines.jl
@@ -38,13 +38,14 @@ end
 """
     IntensityProjection(rec::ReconstructionCPM{T}, params::Params)
 
-Returns a function which does the `intensityProjection`.
+Returns a function which does the `intensityProjection!`.
+The returned function will mutate the input `esw`.
 
  # Examples
 ```julia
-julia> intensityProjection = IntensityProjection(rec, params)
+julia> intensityProjection! = IntensityProjection(rec, params)
 
-julia> eswUpdate = intensityProjection(esw, Imeasured)
+julia> eswUpdate = intensityProjection!(esw, Imeasured)
 ```
 """
 function IntensityProjection(rec::ReconstructionCPM{T}, params::Params) where T
@@ -68,12 +69,8 @@ function IntensityProjection(rec::ReconstructionCPM{T}, params::Params) where T
             Iestimated = let
                 if intensityConstraint === IntensityConstraintStandard
                     # sum over the last three channels.
-                    #@tullio Iestimated[i, j] = abs2(ESW[i,j,k,s1,s2,s3]) 
-                    # that is currently faster than @tullio
                     map!(abs2, abs2_buffer, ESW)
-                    #view(sum!(sum_buffer, abs2_buffer, dims=(3, 4, 5, 6)), :, :, 1,1,1,1)
                     view(sum!(sum_buffer, abs2_buffer), :, :, 1,1,1,1)
-                    # view(sum(abs2, ESW, dims=(3, 4, 5, 6)), :, :, 1,1,1,1)
                 else
                     error("Unknown intensityConstraint $intensityConstraint")
                 end
@@ -81,9 +78,7 @@ function IntensityProjection(rec::ReconstructionCPM{T}, params::Params) where T
 
             frac = let 
                 if intensityConstraint === IntensityConstraintStandard 
-                    #@tullio frac[a1,a2] := sqrt(Imeasured[a1,a2] / (Iestimated[a1,a2] + gimmel))
                      frac_buffer .= sqrt.(Imeasured ./ (Iestimated .+ gimmel))
-                    # frac = sqrt.(Imeasured ./ (Iestimated .+ gimmel))
                 else
                     error("Unknown intensityConstraint")
                 end
@@ -99,4 +94,79 @@ function IntensityProjection(rec::ReconstructionCPM{T}, params::Params) where T
     end
 
     return f! 
+end
+
+
+
+"""
+    _loopUpdatePIE!(<args>)
+
+Internal function which serves as the inner for loop of the PIE based algorithms
+like `ePIE` or `zPIE`.
+    
+"""
+function _loopUpdatePIE!(posList, positions, Np, ptychogram, 
+                         object, oldObjectPatch, oldProbe, probe, esw, DELTA,
+                         intensityProjection!, probeUpdate, objectPatchUpdate)
+    for positionIndex in posList 
+        row, col = positions[:, positionIndex] 
+            
+        sy = row:(row + Np - 1)
+        sx = col:(col + Np - 1)
+        # no copy necessary -> because esw immediately calculated
+        objectPatch = view(object, sy, sx, ..)
+        
+        # save old state since we need that later for probeUpdate and objectPatchUpdate
+        oldObjectPatch .= objectPatch
+        oldProbe .= probe
+        
+        # exit surface wave,
+        esw .= objectPatch .* probe
+        
+        # already store esw in DELTA, since intensityProjection is going to change esw
+        DELTA .= -1 .* esw
+        eswUpdate = intensityProjection!(esw, view(ptychogram, :, :, positionIndex))
+        
+         # difference term
+        DELTA .+= eswUpdate
+        
+        # update newProbe und newObjectPatch
+        newProbe = probeUpdate(oldObjectPatch, oldProbe, DELTA)
+        newObjectPatch = objectPatchUpdate(oldObjectPatch, oldProbe, DELTA)
+        probe .= newProbe
+        object[sy, sx, ..] .= newObjectPatch
+    end 
+end
+
+"""
+    _prepareBuffersAndFunctionsPIE(rec, params, engine)
+
+Prepate buffers and functions needed for PIE based reconstructions
+"""
+function _prepareBuffersAndFunctionsPIE(rec, params, engine)
+    # create intensityProjection function 
+    intensityProjection! = IntensityProjection(rec, params)
+
+    # get two function which maybe shift depending on the flag             \
+    maybe_fftshift, maybe_ifftshift = getMaybeFftshifts(! params.fftshiftFlag) 
+
+    # ptychogram = rec.ptychogram
+    ptychogram = maybe_ifftshift(rec.ptychogram)
+
+    # if that flag is true, swap first and second dimension (transpose)
+    if params.transposePtychogram
+        ptychogram = permutedims(ptychogram, (2,1,3))
+    end
+
+    # those are buffers
+    oldProbe = copy(rec.probe)
+    oldObjectPatch = rec.object[1:rec.Np, 1:rec.Np, ..]
+    # just a buffer with correct shape
+    esw = rec.object[1:rec.Np, 1:rec.Np, ..] .* rec.probe
+    DELTA = similar(esw)
+    # performant update functions for probe and patch without memory usage.
+    objectPatchUpdate, probeUpdate = createUpdateFunctions(engine, oldObjectPatch, oldProbe, DELTA)
+
+    return (intensityProjection!, objectPatchUpdate, probeUpdate, 
+            ptychogram, oldProbe, oldObjectPatch, esw, DELTA)
 end
